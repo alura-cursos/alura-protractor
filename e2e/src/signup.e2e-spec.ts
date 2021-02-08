e51e786e104db14a8cae200f892b5c9d24537b19
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-02-08 23:33:33 +0000

    video5.3

---
+++ e2e/src/signup.e2e-spec.ts
@@ -0,0 +1,37 @@
+import { sign } from 'crypto';
+import { browser, logging } from 'protractor';
+import { SignUpPage } from './signup.po';
+
+describe('SignUp Page', () => {
+
+  let signUpPage: SignUpPage = null;
+
+  afterEach(async () => {
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+        level: logging.Level.SEVERE
+      } as logging.Entry));
+  });
+
+  beforeEach(async () => {
+    signUpPage = new SignUpPage();
+    await signUpPage.navigateTo();
+  });
+
+  it('Should be on Signup Page', async () => {
+    const title = await signUpPage.getTitle();
+    expect(title).toEqual(SignUpPage.PAGE_TITLE);
+  });
+
+  it('Should register a user', async () => {
+    const randomPrefix = Math.round(Math.random() * 100000);
+    await signUpPage.fillEmailField(`email${randomPrefix}@email.com`);
+    await signUpPage.fillFullNameField(`some name ${randomPrefix}`);
+    const userName = `user${randomPrefix}`;
+    await signUpPage.fillUserNameField(userName);
+    const password = '12345678';
+    await signUpPage.fillPasswordField(password);
+    await signUpPage.register();
+  });
+
+});
