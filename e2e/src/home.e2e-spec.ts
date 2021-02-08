6f58b049865df8f363d86fff350ec78933b4e3c1
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-02-08 19:49:12 +0000

    video3.1

--- e2e/src/home.e2e-spec.ts
+++ e2e/src/home.e2e-spec.ts
@@ -2,21 +2,22 @@ import { browser, by, element, protractor } from 'protractor';;
 
 describe('Home Page', () => {
 
-  it('Should navigate to user profile', async () => {
+  beforeEach(async () => {
     await browser.get(`${browser.baseUrl}/#/user/flavio`);
+  });
+
+  it('Should navigate to user profile', async () => {
     const title = await browser.getTitle();
     expect(title).toEqual('Timeline');
   });
 
   it('Should display a list of photos', async () => {
-    await browser.get(`${browser.baseUrl}/#/user/flavio`);
     const list = element.all(by.css('.photo'));
     const photoListSize = await list.count();
     expect(photoListSize).toBeGreaterThan(0);
   });
 
   it('Should navigate to photo detail when photo navigation is triggered', async () => {
-    await browser.get(`${browser.baseUrl}/#/user/flavio`);
     const firstElement = element.all(by.css('.photo')).first();
     await firstElement.sendKeys(protractor.Key.ENTER);
     const title = await browser.getTitle();
@@ -24,7 +25,6 @@ describe('Home Page', () => {
   });
 
   it('Should list one item when filtering by word "farol"', async () => {
-    await browser.get(`${browser.baseUrl}/#/user/flavio`);
     const searchInput = element(by.css('ap-search input[type=search]'));
     await searchInput.sendKeys('farol');
     const list = element.all(by.css('.photo'));
