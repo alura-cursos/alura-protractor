9234e0ce5fd105be255953d317ced54f49ff5d4a
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-02-08 22:30:50 +0000

    video4.3

--- e2e/src/photo-detail.po.ts
+++ e2e/src/photo-detail.po.ts
@@ -1,4 +1,4 @@
-import { browser } from 'protractor';
+import { browser, by, element } from 'protractor';
 
 export class PhotoDetailPage {
 
@@ -6,5 +6,21 @@ export class PhotoDetailPage {
 
   getWindowTitle() {
     return browser.getTitle();
+  }
+
+  fillComment(text: string) {
+    return element(by.css('textarea.form-control'))
+      .sendKeys(text);
+  }
+
+  publishComment() {
+    return element(by.css('button[type=submit]'))
+      .click();
+  }
+
+  getCommentListSize() {
+    return element
+      .all(by.css('ap-photo-comments li'))
+      .count();
   }
 }
